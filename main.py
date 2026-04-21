6b075babd50e2b1cf58662cb3011a913fdd17869
serukesxz-max <serukesxz@gmail.com>
2026-04-22 00:05:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot pornit ca {bot.user}")
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+  bot.run(os.getenv("TOKEN"))
